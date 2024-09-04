6d13c56d5f5f7674e918c6d8a8c796cd420f4c30
Nicholas1Front <nicholasdeoliveiraeugenio@gmail.com>
2024-09-03 23:50:16 -0300

    created add clients function

--- apps/customer_base_plataform/scripts/script.js
+++ apps/customer_base_plataform/scripts/script.js
@@ -22,3 +22,32 @@ async function getClientsData(){
 }
 
 getClientsData();
+
+//add-client-section
+
+//elements
+const addClientInput = document.querySelector("#add-client-input");
+const addClientBtn = document.querySelector("#add-client-btn");
+//functions
+
+function addClientProcess(){
+
+    console.log(clients_equipaments_array);
+
+    const newClient = {
+        name : addClientInput.value,
+        equipaments : []
+    }
+
+    clients_equipaments_array.push(newClient);
+
+    console.log(clients_equipaments_array);
+    
+    return clients_equipaments_array;
+}
+
+// event listerner
+
+addClientBtn.addEventListener("click", (event)=>{
+    addClientProcess();
+});
