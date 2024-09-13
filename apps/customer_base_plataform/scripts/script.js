be222f7e1eb1726cf72e0e1de05e58ca04477be1
Nicholas1Front <nicholasdeoliveiraeugenio@gmail.com>
2024-09-13 00:36:54 -0300

    deleted updateJson

--- apps/customer_base_plataform/scripts/script.js
+++ apps/customer_base_plataform/scripts/script.js
@@ -45,25 +45,8 @@ getClientsData();
 //update json 
 
 //elements
-let GITHUB_TOKEN = null;
-const REPO_OWNER = 'Nicholas1Front'; // Dono do repositório
-const REPO_NAME = 'test_repo'; // Nome do repositório
-const FILE_PATH = 'clients_equipaments.json'; // Caminho do arquivo JSON no repositório
-const BRANCH = 'main';
 
 //functions
-
-async function getTokenLocal(){
-    const response = await fetch("../data/token_json.json");
-
-    const token = await response.json();
-
-    GITHUB_TOKEN = token;
-
-    return GITHUB_TOKEN
-}
-
-getTokenLocal();
 
 async function getFileSha() {
     const url = `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/${FILE_PATH}?ref=${BRANCH}`;
